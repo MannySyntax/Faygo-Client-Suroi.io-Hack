7c7f3862b3efbd2e9558ca98eaaff8d08397d2f6
Manny <109997233+MannySyntax@users.noreply.github.com>
2024-02-03 21:26:06 -0500

    Create module.js

---
+++ module.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Faygo Client - Suroi
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-02
+// @description  hi skid
+// @author       MannyCodes
+// @include      *
+// @grant        none
+// @run-at       document-start
+// @icon         https://i.etsystatic.com/24406703/r/il/ae4414/2493279816/il_fullxfull.2493279816_hl5x.jpg
+// @require      https://code.jquery.com/jquery-3.7.0.min.js
+// @require      https://gist.githubusercontent.com/MannySyntax/5eb3ba185d726c799e2bf16a770a1ff7/raw/6908e7fbd8d00041f8d17b03a0c013af923cef8b/Faygo.js
+// ==/UserScript==
+
+
+/*
+
+███████╗ █████╗ ██╗   ██╗ ██████╗  ██████╗
+██╔════╝██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔═══██╗
+█████╗  ███████║ ╚████╔╝ ██║  ███╗██║   ██║
+██╔══╝  ██╔══██║  ╚██╔╝  ██║   ██║██║   ██║
+██║     ██║  ██║   ██║   ╚██████╔╝╚██████╔╝
+╚═╝     ╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝
+
+How Do I Install?
+ - First of all everything needed is loaded from @require and to install the hack you need to paste this code into a new tampermonkey and press save!
+ - Congrats! Let's make this game the new surviv! ❤️
+ - Make sure to share with your friends so everyone can start cheating!
+
+STATUS: WORKING
+
+Author: MannyCodes
+Discord: m.otion
+
+Features
+ - Aimbot
+ - Tracers
+ - Zoom
+ - Hide Gas
+*/
